ad4787fd7259027c5d40853aaf3e01b121f72308
Anton <anton_sonic@inbox.ru>
2022-05-30 15:50:11 +0300

    Updates

---
+++ precache-manifest.62d28b750d6a6c1dfa87d19644f9feab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a23a864d3a79c7a7acdf413d284408",
+    "url": "/movie-db-api/index.html"
+  },
+  {
+    "revision": "01503cdcced6c77840a4",
+    "url": "/movie-db-api/static/css/main.76cc0ec1.chunk.css"
+  },
+  {
+    "revision": "9f50bf37857d8092d8ca",
+    "url": "/movie-db-api/static/js/2.1618b2c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-db-api/static/js/2.1618b2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01503cdcced6c77840a4",
+    "url": "/movie-db-api/static/js/main.e6c3ff76.chunk.js"
+  },
+  {
+    "revision": "4821ab08687df91f50e7",
+    "url": "/movie-db-api/static/js/runtime-main.de274c52.js"
+  },
+  {
+    "revision": "9f61111962cc66e53f0c907d4fcc17cb",
+    "url": "/movie-db-api/static/media/heart-fill.9f611119.png"
+  },
+  {
+    "revision": "64830ae98f9b3327fed4f053d76d589c",
+    "url": "/movie-db-api/static/media/johnny-depp-tears.64830ae9.gif"
+  },
+  {
+    "revision": "01ef2131d1d741dfed65fd9546c0dbe6",
+    "url": "/movie-db-api/static/media/like.01ef2131.png"
+  }
+]);
